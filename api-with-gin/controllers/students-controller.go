184660ae9eb041a0e6705f17e0e54f81ca34adf5
Davi Silva <davisilvaphoto@gmail.com>
2022-03-16 09:35:41 -0300

    get routes params

--- api-with-gin/controllers/students-controller.go
+++ api-with-gin/controllers/students-controller.go
@@ -1,10 +1,22 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func ShowAllStudents(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"id":   "1",
 		"name": "Davi Silva",
+	})
+}
+
+func Gretting(c *gin.Context) {
+	name := c.Params.ByName("name")
+
+	c.JSON(200, gin.H{
+		"APY says:": "Hello " + strings.Title(strings.ToLower(name)) + ", are you ok ?",
 	})
 }
--- api-with-gin/routes/routes.go
+++ api-with-gin/routes/routes.go
@@ -10,6 +10,7 @@ func HandleRequests() {
 	r := gin.Default()
 
 	r.GET("/students", controllers.ShowAllStudents)
+	r.GET("/:name", controllers.Gretting)
 
 	r.Run()
 }
